9777735ee5cda9507125e014a2ef1594ea238327
skawls <ventu9204@gmail.com>
2023-08-11 18:56:59 +0900

    1주차 과제

---
+++ vite-project/src/1차과제/TrafficLight.jsx
@@ -0,0 +1,13 @@
+import styles from "@/styles/TraffcLight.module.css";
+
+function TraffcLight() {
+  return (
+    <>
+      <form className={styles.container}>
+      <small>멈춰!</small>
+      </form>
+    </>
+  );
+}
+
+export default TraffcLight;
